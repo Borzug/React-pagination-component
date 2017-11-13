6c3dde554a1ed5b0729b077812ceda204e741656
Denis <tendomail@mail.ru>
2017-11-13 19:23:55 +0300

    initial commit

---
+++ Pagination.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const NUMBER_OF_BUTTONS = 5;
+
+function Pagination ({ pagesCount, currentPage, changePage }) {
+    const paginationLength = pagesCount <= NUMBER_OF_BUTTONS ? pagesCount : NUMBER_OF_BUTTONS;
+
+    const getPaginationStart = (count, current) => {
+        if (current >= Math.ceil(NUMBER_OF_BUTTONS / 2) && current <= count - Math.floor(NUMBER_OF_BUTTONS / 2)) {
+            return current - Math.floor(NUMBER_OF_BUTTONS / 2);
+        }
+        if (current < Math.ceil(NUMBER_OF_BUTTONS / 2)) {
+            return 1;
+        }
+        if (current > count - Math.floor(NUMBER_OF_BUTTONS / 2)) {
+            return count - paginationLength + 1;
+        }
+    }    
+    const pages = Array.from({ length: paginationLength }, (page, idx) => page = getPaginationStart(pagesCount, currentPage) + idx);
+    const active = (page) => {
+        return currentPage === page ? "page-item active" : "page-item";
+    }
+    const previous = currentPage > 1 ? currentPage - 1 : 1;
+    const next = currentPage < pagesCount ? currentPage + 1 : pagesCount;    
+    return (
+        <div>
+            <nav aria-label="...">
+                <ul className="pagination pagination-sm justify-content-center rounded-0">
+                    <li className="page-item">
+                        <a id="1" className="page-link" onClick={changePage} >{'<<'}</a>
+                    </li>
+                    <li className="page-item">
+                        <a id={previous} className="page-link" onClick={changePage} >Предыдущая</a>
+                    </li>
+                        {pages.map((page, idx) => (
+                            <li key={idx} className={active(page)}><a id={page} className="page-link" onClick={changePage}>{page}</a></li>
+                        ))}                    
+                    <li className="page-item">
+                        <a id={next} className="page-link" onClick={changePage} >Следующая</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Pagination;
