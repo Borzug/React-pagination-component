e958d757d654c15941244f4fe501b8958cffe038
Denis <tendomail@mail.ru>
2017-11-15 00:15:04 +0300

    Update Pagination.jsx

--- Pagination.jsx
+++ Pagination.jsx
@@ -21,25 +21,30 @@ function Pagination ({ pagesCount, currentPage, changePage }) {
         return currentPage === page ? "page-item active" : "page-item";
     }
     const previous = currentPage > 1 ? currentPage - 1 : 1;
-    const next = currentPage < pagesCount ? currentPage + 1 : pagesCount;    
+    const next = currentPage < pagesCount ? currentPage + 1 : pagesCount;
+    const nextButtonClass = currentPage === pagesCount ? "page-item disabled" : "page-item";
+    const previousButtonClass = currentPage === 1 ? "page-item disabled" : "page-item";
     return (
         <div>
             <nav aria-label="...">
-                <ul className="pagination pagination-sm justify-content-center rounded-0">
-                    <li className="page-item">
-                        <a id="1" className="page-link" onClick={changePage} >{'<<'}</a>
-                    </li>
-                    <li className="page-item">
+                <ul className="pagination pagination-sm justify-content-center">
+                    <li className={previousButtonClass}>
+                        <a id="1" className="page-link" onClick={changePage} >{"<<"}</a>
+                    </li>                   
+                    <li className={previousButtonClass}>
                         <a id={previous} className="page-link" onClick={changePage} >Previous</a>
                     </li>
                         {pages.map((page, idx) => (
                             <li key={idx} className={active(page)}><a id={page} className="page-link" onClick={changePage}>{page}</a></li>
                         ))}                    
-                    <li className="page-item">
+                    <li className={nextButtonClass}>
                         <a id={next} className="page-link" onClick={changePage} >Next</a>
                     </li>
+                    <li className={nextButtonClass}>
+                        <a id={pagesCount} className="page-link" onClick={changePage} >{">>"}</a>
+                    </li>
                 </ul>
-            </nav>
+            </nav>            
         </div>
     )
 }
