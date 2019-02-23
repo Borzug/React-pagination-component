408d69bd86ae5d9109f55241b47193c9f37c4489
Borzug <lycobora@gmail.com>
2019-02-23 22:11:19 +0300

    TypeScript

---
+++ Pagination.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+
+import { PaginationLink } from "./PaginationLink";
+
+const NUMBER_OF_BUTTONS = 5;
+
+interface IProps {
+    pagesCount: number;
+    currentPage: number;
+    changePage: (pageNumber: number) => void;
+}
+
+export class Pagination extends React.PureComponent<IProps> {
+    public render() {
+        const { pagesCount, currentPage, changePage } = this.props;
+
+        const paginationLength = pagesCount <= NUMBER_OF_BUTTONS ? pagesCount : NUMBER_OF_BUTTONS;
+
+        const pages = Array.from({ length: paginationLength }, (_, idx) => {
+            return this.getPaginationStart(pagesCount, currentPage, paginationLength) + idx;
+        });
+
+        const previous = currentPage > 1 ? currentPage - 1 : 1;
+        const next = currentPage < pagesCount ? currentPage + 1 : pagesCount;
+        const nextButtonClass = currentPage === pagesCount ? "page-item disabled" : "page-item";
+        const previousButtonClass = currentPage === 1 ? "page-item disabled" : "page-item";
+
+        return (
+            <div>
+                <nav aria-label="...">
+                    <ul className="pagination pagination-sm justify-content-center">
+                        <li className={previousButtonClass}>
+                            <PaginationLink value={1} text={"<<"} changePage={changePage} />
+                        </li>
+
+                        <li className={previousButtonClass}>
+                            <PaginationLink value={previous} text="Предыдущая" changePage={changePage} />
+                        </li>
+
+                        {pages.map((page, idx) => (
+                            <li key={idx} className={this.getButtonClass(page)}>
+                                <PaginationLink value={page} changePage={changePage} />
+                            </li>
+                        ))}
+
+                        <li className={nextButtonClass}>
+                            <PaginationLink value={next} text="Следующая" changePage={changePage} />
+                        </li>
+
+                        <li className={nextButtonClass}>
+                            <PaginationLink value={pagesCount} text={">>"} changePage={changePage} />
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+
+    private getPaginationStart = (count: number, current: number, paginationLength: number) => {
+        if (current >= Math.ceil(NUMBER_OF_BUTTONS / 2) && current <= count - Math.floor(NUMBER_OF_BUTTONS / 2)) {
+            return current - Math.floor(NUMBER_OF_BUTTONS / 2);
+        }
+
+        if (current < Math.ceil(NUMBER_OF_BUTTONS / 2)) {
+            return 1;
+        }
+
+        if (current > count - Math.floor(NUMBER_OF_BUTTONS / 2)) {
+            return count - paginationLength + 1;
+        }
+
+        return 1;
+    }
+
+    private getButtonClass = (page: number) => {
+        return this.props.currentPage === page ? "page-item active" : "page-item";
+    }
+}
